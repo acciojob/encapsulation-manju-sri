7276bab89b8f29d4b5d3bf5893caf5051b41e450
Manjula <manjumanohar0704@gmail.com>
2023-03-21 12:15:51 +0530

    assignment completed

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,17 @@
 package com.driver;
 
+import java.sql.SQLOutput;
+
 public class RWOnly {
-    
+    String name;
+    public String getName() {
+        System.out.println(name);
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
 }
